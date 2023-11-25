92764e4a145d957dd600dec763fe0a1e307980dd
mouaadh11 <mouaadhsahailia@gmail.com>
2023-11-26 00:23:18 +0100

    fix allocation prblm

--- microcontroller/new.py
+++ microcontroller/new.py
@@ -1,58 +1,11 @@
-# # main.py
-import time
-# import sensor
-import connection
-import time_ms
-# def record_to_csv(filename, data):
-#     with open(filename, "a") as file:
-#         file.write(data + "\n")
-
-# def main():
-#     sensor1, default_value = sensor.setUp()
-#     cpt = 1
-#     start_recording = time.ticks_ms()
-#     print ("start_recording", start_recording)
-#     csv_filename = "wrist_MED_2.csv"
-#     with open(csv_filename, "a") as file:
-#         file.write("hr,red,datetime\n")
-#     while True:
-        
-#         ir_reading, red_reading = sensor.get_data(sensor1, default_value)
-#         # Get the current time
-#         current_time = time_ms.get_time()
-#         # Record data to CSV
-#         record_to_csv(csv_filename, f"{ir_reading},{red_reading},{current_time}")
-
-#         #print("ir_reading = ", ir_reading)
-#         #print("red_reading = ", red_reading)
-#         #second_check = time.localtime()[5]
-#         #print(time.ticks_diff(time.ticks_ms(), start_recording))
-#         # Send data to the server every 60 seconds
-#         if time.ticks_diff(time.ticks_ms(), start_recording) >= 60000:
-#             start_recording = time.ticks_ms()
-#             print ("dkhalt")
-#             try: 
-#                 connection.send_data(csv_filename)
-#                 cpt += 1
-#             except OSError:
-#                 print("Error while sending data to server")
-#             return 0
-            
-#             #time.sleep(0.5)
-
-# if __name__ == '__main__':
-#     main()
 import time
 import connection
 import time_ms
-import uos
 from machine import sleep, SoftI2C, Pin
 from utime import ticks_diff, ticks_us
-import gc
 from max30102 import MAX30102, MAX30105_PULSE_AMP_MEDIUM
 
-# main.py
-# gc.enable()
+
 def main():
     csv_filename = "samples-try.csv"
     with open(csv_filename, "a") as file:
@@ -84,16 +37,17 @@ def main():
     else:
         print("Sensor connected and recognized.")
 
-    # It's possible to set up the sensor at once with the setup_sensor() method.
-    # If no parameters are supplied, the default config is loaded:
-    # Led mode: 2 (RED + IR)
-    # ADC range: 16384
-    # Sample rate: 400 Hz
-    # Led power: maximum (50.0mA - Presence detection of ~12 inch)
-    # Averaged samples: 8
-    # pulse width: 411
+    # Set up the sensor
     print("Setting up sensor with default configuration.", '\n')
     sensor.setup_sensor()
+
+    # It is also possible to tune the configuration parameters one by one.
+    # Set the sample rate to 400: 400 samples/s are collected by the sensor
+    sensor.set_sample_rate(400)
+    # Set the number of samples to be averaged per each reading
+    sensor.set_fifo_average(8)
+    # Set LED brightness to a medium value
+    sensor.set_active_leds_amplitude(MAX30105_PULSE_AMP_MEDIUM)
 
     sleep(1)
 
@@ -107,8 +61,8 @@ def main():
     print("Starting data acquisition from RED & IR registers...", '\n')
     sleep(1)
 
-    readings_list_total = [[None, None, None] for i in range(1100)]
-    cpt = 0
+    readings_list_it = []
+    readings_list_total = []
     t_start = ticks_us()  # Starting time of the acquisition
     start_recording = time.ticks_ms()
     samples_n = 0  # Number of samples that have been collected
@@ -121,8 +75,6 @@ def main():
 
         # Check if the storage contains available samples
         if sensor.available():
-            # print ("(before writing) the free memory is: ", gc.mem_free(), "bytes")
-
             # Access the storage FIFO and gather the readings (integers)
             
             red_reading = sensor.pop_red_from_storage()
@@ -130,11 +82,16 @@ def main():
             
             # Get the current time
             current_time = time_ms.get_time()
+            readings_list_it.append(red_reading)
+            readings_list_it.append(ir_reading)
+            readings_list_it.append(current_time)
+
+            readings_list_total.append(readings_list_it)
+            print (readings_list_total)
+            readings_list_it.clear()
+            print (len(readings_list_it))
+            print (len(readings_list_total))
             
-            readings_list_total[cpt][0]= red_reading
-            readings_list_total[cpt][1]= ir_reading
-            readings_list_total[cpt][2]= current_time
-            cpt = cpt + 1
             # Compute the real frequency at which we receive data
             if compute_frequency:
                 if ticks_diff(ticks_us(), t_start) >= 999999:
@@ -146,17 +103,13 @@ def main():
                     samples_n = samples_n + 1
 
             if time.ticks_diff(time.ticks_ms(), start_recording) >= 20000:
-                print ("dkhalt")
-                #print (readings_list_total)
-                with open(csv_filename, "a") as file:
-                     for i in range(cpt):
-                        file.write( f"{readings_list_total[i][1]},{readings_list_total[i][0]},{readings_list_total[i][2]}"+ '\n')
-                cpt = 1
-                gc.collect()
-                uos.remove(csv_filename)
                 start_recording = time.ticks_ms()
-                #print(readings_list_total)
-                #send data to server
+                print ("dkhalt")
+                print (readings_list_total)
+                with open(csv_filename, "a") as file:
+                     for reading in readings_list_total:
+                        file.write( f"{reading[1]},{reading[0]},{reading[2]}"+ '\n')
+                return 0
 
 
 if __name__ == '__main__':
